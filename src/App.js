d85ecd12566e49ba924521ef60d271927b4da794
alex <win32coder@list.ru>
2017-03-24 18:38:16 +0300

    добавил ссылки на объекты bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,72 @@
 import React, { Component } from 'react';
+import { Grid, Form, FormControl, FormGroup, Panel, PanelGroup, Navbar, Glyphicon,
+  Nav, NavItem, Well, Row, Col, Button } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
+
+const gridInstance = (
+  <Grid>
+    <Row className="show-grid">
+      <Col xs={6} md={2}></Col>
+      <Col xs={12} md={8}>
+        <Form>
+          <FormControl type="text" placeholder="Search products" />
+        </Form>
+      </Col>
+      <Col xs={6} md={2}></Col>
+    </Row>
+  </Grid>
+);
+
+
+class HeadPanel extends Component {
+  render(){
+    return (
+      <Panel>
+        {gridInstance}
+      </Panel>
+    );
+  }
+}
+class ProgressPanel extends Component {
+  render(){
+    return (
+      <Panel>
+        <Grid>
+          <Row className="show-grid">
+            <Col md={8}>
+            </Col>
+          </Row>
+        </Grid>
+      </Panel>
+    );
+  }
+}
+class MainPanel extends Component {
+  render(){
+    return (
+      <Panel>
+        <Grid>
+          <Row className="show-grid">
+            <Col md={8}>
+            </Col>
+          </Row>
+        </Grid>
+      </Panel>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="ReactEshop__nav-spacer" />
+        <PanelGroup>
+          <HeadPanel />
+          <ProgressPanel />
+          <MainPanel />
+        </PanelGroup>
       </div>
     );
   }
