efe5ac42a7488e6cf846172f8ba8ae02055d70d2
alex <win32coder@list.ru>
2017-03-27 20:50:23 +0300

    добавил дерево в левую панель

--- src/LeftPanel.jsx
+++ src/LeftPanel.jsx
@@ -1,19 +1,35 @@
 import React, {Component} from 'react';
 import { Grid, Form, FormControl, FormGroup, Panel, PanelGroup, Navbar, Glyphicon,
   Nav, NavItem, Well, Row, Col, Button, ListGroup, ListGroupItem, ProgressBar } from 'react-bootstrap';
+import SortableTree from 'react-sortable-tree';
+
+class Tree extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            treeData: [{ title: 'Chicken', children: [ { title: 'Egg', children:[{title:"e1"}, {title:"e2"}] } ] }],
+        };
+    }
+
+    render() {
+        return (
+            <div style={{ height: 400 }}>
+                <SortableTree
+                    treeData={this.state.treeData}
+                    onChange={treeData => this.setState({ treeData })}
+                />
+            </div>
+        );
+    }
+}
 
 
 class LeftPanel extends React.Component{
   render(){
     return (
       <Panel >
-        Some default panel content here.
-        <ListGroup fill>
-          <ListGroupItem>Item 1</ListGroupItem>
-          <ListGroupItem>Item 2</ListGroupItem>
-          <ListGroupItem>&hellip;</ListGroupItem>
-        </ListGroup>
-        Some more panel content here.
+        <Tree />
       </Panel>
     );
   }
